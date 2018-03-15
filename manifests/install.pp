de6667daf1675cfe552937c747204964ef47505f
Lee Briggs <lbriggs@apptio.com>
2018-03-15 12:00:06 -0700

    Add ability to manage kubelet, kubectl

--- manifests/install.pp
+++ manifests/install.pp
@@ -8,4 +8,16 @@ class kubeadm::install {
     ensure => $kubeadm::package_ensure,
   }
 
+  if $kubeadm::manage_kubelet {
+    package { $kubeadm::kubelet_package_name:
+      ensure => $kubeadm::kubelet_package_ensure,
+    }
+  }
+
+  if $kubeadm::manage_kubectl {
+    package { $kubeadm::kubectl_package_name:
+      ensure => $kubeadm::kubectl_package_ensure,
+    }
+  }
+
 }
