31b7bc7543e7b4b26b13fb95e6ed59a2fa313c1e
Lee Briggs <lbriggs@apptio.com>
2018-03-23 08:55:30 -0700

    Notify the kubelet on join

--- manifests/node.pp
+++ manifests/node.pp
@@ -4,12 +4,18 @@
 #
 class kubeadm::node {
 
+  $notify_on_change = $kubeadm::manage_kubelet ? {
+    true    => Service['kubelet'],
+    default => undef,
+  }
+
   # if the bootstrapped fact isn't set
   # join the cluster
   if ! $::kubeadm_bootstrapped {
     exec { 'kubeadm join':
       command => "kubeadm join --config ${kubeadm::config_dir}/config.json",
       path    => '/usr/bin:/usr/local/bin:/usr/sbin:/sbin',
+      notify  => $notify_on_change,
     }
   }
 }
