619ce1662f7eff735312d1dd9a3c2cf6e074385e
Sanyu Melwani <1154057+sanyu@users.noreply.github.com>
2018-06-26 15:48:41 +1000

    Refactored some code

--- manifests/master.pp
+++ manifests/master.pp
@@ -6,23 +6,21 @@ class kubeadm::master (
   $refresh_controlplane,
 ){
 
-  $subscribe = $refresh_controlplane ? {
-    true    => File['kubeadm config.json'],
-    default => undef,
-  }
   # if we are a master, install the components we need to update the controlplane
   # every time the config file changes
-  $refresh_controplane = "kubeadm alpha phase controlplane all --config ${::kubeadm::config_dir}/config.json && sleep 10"
-  exec { 'kubeadm controlplane':
-    command     => $refresh_controplane,
-    path        => '/usr/bin:/usr/local/bin:/usr/sbin:/sbin',
-    subscribe   => $subscribe,
-    refreshonly => true,
-  }
-  ~> exec { 'kubeadm kubeconfig':
-    command     => "kubeadm alpha phase kubeconfig all --config ${kubeadm::config_dir}/config.json",
-    path        => '/usr/bin:/usr/local/bin:/usr/sbin:/sbin',
-    refreshonly => true,
+  if $refresh_controlplane {
+    $controplane_cmd = "kubeadm alpha phase controlplane all --config ${::kubeadm::config_dir}/config.json && sleep 10"
+    exec { 'kubeadm controlplane':
+      command     => $controplane_cmd,
+      path        => '/usr/bin:/usr/local/bin:/usr/sbin:/sbin',
+      subscribe   => File['kubeadm config.json'],
+      refreshonly => true,
+    }
+    ~> exec { 'kubeadm kubeconfig':
+      command     => "kubeadm alpha phase kubeconfig all --config ${kubeadm::config_dir}/config.json",
+      path        => '/usr/bin:/usr/local/bin:/usr/sbin:/sbin',
+      refreshonly => true,
+    }
   }
 
   exec{'kubeadm init':
